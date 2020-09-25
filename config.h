b38bc6f266c95de45477e642c3225eae08d0cbfc
nho1ix <tupilbewb@gmail.com>
2020-09-25 08:45:11 -0700

    Got rid of parentheses around instagram-nativefier

--- config.h
+++ config.h
@@ -273,7 +273,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_F2,		spawn,		SHCMD("tutorialvids") },
 	{ MODKEY,			XK_F3,		spawn,		SHCMD("displayselect") },
 	/* { MODKEY,			XK_F4,		spawn,          SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") }, */
-	{ MODKEY,			XK_F4,		spawn,          SHCMD("(instagram-nativefier)") },
+	{ MODKEY,			XK_F4,		spawn,          SHCMD("instagram-nativefier") },
 	/* { MODKEY,			XK_F5,		xrdb,		{.v = NULL } }, */
 	{ MODKEY,			XK_F5,		spawn,		SHCMD("discord") },
 	{ MODKEY,			XK_F6,		spawn,		SHCMD("torbrowser-launcher") },
