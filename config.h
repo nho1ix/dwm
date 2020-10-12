71432965569d1a64dbae0544de8cdcaed35d9930
nho1ix <tupilbewb@gmail.com>
2020-10-12 00:41:02 -0700

    border pixel update

--- config.h
+++ config.h
@@ -6,7 +6,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+// static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 6;       /* snap pixel */
 static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
