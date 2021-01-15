d5dcf15621f3ce0e5c74db98e2628e1f41292e19
nho1ix <tupilbewb@gmail.com>
2021-01-15 15:34:39 -0800

    add keybinds

--- config.h
+++ config.h
@@ -304,12 +304,12 @@ static Key keys[] = {
 	{ MODKEY,			XK_F1,		spawn,		SHCMD("groff -mom /usr/local/share/dwm/larbs.mom -Tpdf | zathura -") },
 //      { MODKEY,			XK_F2,		spawn,		SHCMD("tutorialvids") },
 //      { MODKEY,			XK_F3,		spawn,		SHCMD("displayselect") },
-	{ MODKEY,			XK_F3,		spawn,          SHCMD("whatsapp-nativefier") },
+	{ MODKEY,			XK_F3,		spawn,          SHCMD("signal-desktop-beta") },
 	/* { MODKEY,			XK_F4,		spawn,          SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") }, */
 	{ MODKEY,			XK_F4,		spawn,          SHCMD("instagram-nativefier") },
 	/* { MODKEY,			XK_F5,		xrdb,		{.v = NULL } }, */
 	{ MODKEY,			XK_F5,		spawn,		SHCMD("lightcord") },
-	{ MODKEY,			XK_F6,		spawn,		SHCMD("torbrowser-launcher") },
+	{ MODKEY,			XK_F6,		spawn,		SHCMD("tor-browser") },
 	/* { MODKEY,			XK_F6,		spawn,		SHCMD("torwrap") }, */
 	/* { MODKEY,			XK_F7,		spawn,		SHCMD("td-toggle") }, */
 	{ MODKEY,			XK_F7,		spawn,		SHCMD("caprine") },
