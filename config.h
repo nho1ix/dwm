378cbef1ac486a13a5d480d2258540f7c89614de
nho1ix <tupilbewb@gmail.com>
2021-06-06 00:07:57 -0700

    Fix signal-desktop-beta F3 binding (comment out)

--- config.h
+++ config.h
@@ -305,7 +305,7 @@ static Key keys[] = {
 //        { MODKEY,			XK_F2,		spawn,		SHCMD("spotify") },
         { MODKEY,			XK_F3,		spawn,		SHCMD("sct 3000 && sh ~/.config/scripts/sct.sh") },
 //        { MODKEY,			XK_F3,		spawn,		SHCMD("displayselect") },
-	{ MODKEY,			XK_F3,		spawn,          SHCMD("signal-desktop-beta") },
+	//{ MODKEY,			XK_F3,		spawn,          SHCMD("signal-desktop-beta") },
 	/* { MODKEY,			XK_F4,		spawn,          SHCMD(TERMINAL " -e pulsemixer; kill -44 $(pidof dwmblocks)") }, */
 	{ MODKEY,			XK_F4,		spawn,          SHCMD("instagram-nativefier") },
 	/* { MODKEY,			XK_F5,		xrdb,		{.v = NULL } }, */
