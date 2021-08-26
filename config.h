631d83df9ce771ed36e77dced04f08bcb96368f6
nho1ix <tupilbewb@gmail.com>
2021-08-25 21:42:53 -0700

    Migrate to NvChad + Telegram keybind

--- config.h
+++ config.h
@@ -274,7 +274,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_b,		togglebar,	{0} },
 	{ MODKEY|ShiftMask,		XK_b,		spawn,	        SHCMD("brave") },
 	/* { MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("") }, */
-	{ MODKEY,			XK_n,		spawn,		SHCMD(TERMINAL " -e vim ~/vimwiki/index.wiki") },
+	{ MODKEY,			XK_n,		spawn,		SHCMD(TERMINAL " -e nvim ~/vimwiki/index.wiki") },
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat; pkill -RTMIN+6 dwmblocks") },
 	{ MODKEY,			XK_m,		spawn,		SHCMD(TERMINAL " -e mocp") },
 	{ MODKEY|ShiftMask,		XK_m,		spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") },
@@ -315,6 +315,7 @@ static Key keys[] = {
 	/* { MODKEY,			XK_F7,		spawn,		SHCMD("td-toggle") }, */
 	{ MODKEY,			XK_F7,		spawn,		SHCMD("caprine") },
 	/* { MODKEY,			XK_F8,		spawn,		SHCMD("mailsync") }, */
+	{ MODKEY,			XK_F8,		spawn,		SHCMD("telegram-desktop") },
 	/* { MODKEY,			XK_F9,		spawn,		SHCMD("dmenumount") }, */
 //	{ MODKEY,			XK_F10,		spawn,		SHCMD("betterlockscreen -s") },
 	/* { MODKEY,			XK_F10,		spawn,		SHCMD("sudo systemctl suspend && slock") }, */
