7df131397db1285c51c2437088651bfd376efa2f
Victor Vo <from.victor.vo@gmail.com>
2021-09-27 15:45:25 -0700

    Update dwm

--- config.h
+++ config.h
@@ -18,12 +18,15 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "Product Sans:size=11:antialias=true:autohint=true",
+static int usealtbar          = 1;        /* 1 means use non-dwm status bar */
+//static char *altbarclass      = "Polybar"; /* Alternate bar class name */
+//static char *altbarcmd        = "$HOME/.config/polybar/launch.sh"; /* Alternate bar launch command */
+//static char *fonts[]          = { "Product Sans:size=11:antialias=true:autohint=true",
 //                                        "Roboto Mono=11:antialias=true:autohint=true",
 //                                        "JetBrainsMonoNL-Bold:size=9:antialias=true:autohint=true",
 //                                        "NotoColorEmoji:pixelsize=11:antialias=true:autohint=true",
 //                                        "JoyPixels:pixelsize=11:antialias=true:autohint=true", 
-                                        "JetBrainsMono Nerd Font:style=medium:pixelsize=12"
+                                        "JetBrainsMono Nerd Font:style=regular:pixelsize=12"
 //                                        "Hermit:pixelsize=12:antialias=true:autohint=true" 
                                         };
 // static char normbgcolor[]           = "#222222"; // default
