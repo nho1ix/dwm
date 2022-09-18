2cc24bded15531e3179c2dfe5af9e5850c2ff9e2
Victor Vo <from.victor.vo@gmail.com>
2022-09-17 23:05:55 -0700

    Fix glyph size + name

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int smartgaps          = 0;        /* 1 means no outer gap when there is 
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
 static char *fonts[]          = { "Product Sans:size=14:antialias=true:autohint=true",
-                                  "JetBrainsMono Nerd Font Mono:size=15:antialias=true:autohint=true:style=Regular"};
+                                  "JetBrainsMono Nerd Font:size=12:antialias=true:autohint=true:style=Regular"};
 static char normbgcolor[]           = "#000000"; // default
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
