139a539747d9f9a84ec814863877506322f849d1
Victor Vo <from.victor.vo@gmail.com>
2022-10-06 22:48:26 -0700

    Modify font

--- config.h
+++ config.h
@@ -16,8 +16,9 @@ static int swallowfloating    = 0;        /* 1 means swallow floating windows by
 static int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
-static char *fonts[]          = { "Overpass:style=Regular:size=15:antialias=true:autohint=true", 
-                                  "JetBrainsMono Nerd Font:style=regular:pixelsize=12" };
+static char *fonts[]          = { "Product Sans:size=14:antialias=true:autohint=true", 
+                                  "JetBrainsMono Nerd Font:size=12:antialias=true:autohint=true:style=Regular",
+                                  "Font Awesome 6 Free,Font Awesome 6 Free Regular:style=Regular"};
 static char normbgcolor[]           = "#000000"; // default
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#FFFFFF";
