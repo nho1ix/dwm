c50e2a7c3c8200dbc8425e9c269666ad882ee352
Victor Vo <tupilbewb@gmail.com>
2022-11-01 19:29:02 -0700

    Add spotify to tagset (with spotifywm-git)

--- config.h
+++ config.h
@@ -83,6 +83,7 @@ static const Rule rules[] = {
 	{ "Gimp",     NULL,       NULL,       	    0,            0,           0,         0,         0,        -1 },
 	{ TERMCLASS,   NULL,       NULL,       	    0,            0,           0,         1,         0,        -1 },
 	{ "instagram-nativefier", NULL,   NULL,     1 << 3,       0,           0,         0,         0,        -1 },
+	{ "Spotify",  NULL,       NULL,             1 << 6,       0,           0,         0,         0,        -1 },
 	{ "discord",  NULL,       NULL,             1 << 3,       0,           0,         0,         0,        -1 },
 	{ "St",       NULL,       NULL,       	    0,            0,           0,         1,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
