0b75ccefcfb4ebe99ab5daebf366828549393e33
Victor Vo <fromvictorvo@gmail.com>
2024-04-04 03:09:42 -0700

    Added weechat spawn tag 5

--- config.h
+++ config.h
@@ -19,7 +19,7 @@ static unsigned int gappoh    = 0;       /* horiz outer gap between windows and 
 static unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge. default=20 */
 static int swallowfloating    = 0;       /* 1 means swallow floating windows by default */
 static int smartgaps          = 0;       /* 1 means no outer gap when there is only one window */
-static int showbar            = 0;       /* 0 means no bar */
+static int showbar            = 1;       /* 0 means no bar */
 static int topbar             = 1;       /* 0 means bottom bar */
 static int mon_refresh 				= 165;     /* monitor refresh rate */
 static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
@@ -87,6 +87,7 @@ static const Rule rules[] = {
 	{ "discord",  NULL,       NULL,             1 << 3,       0,           0,         0,         0,        1 },
 	{ "obs",      NULL,       NULL,       	    1 << 8,       0,           0,         1,         0,        1 },
 	{ "obsidian",      NULL,       NULL,   	    1 << 4,       0,           0,         1,         0,        -1 },
+	{ "St",       NULL,       "weechat",       	    1 << 4,            0,           0,         1,         0,        1 },
 	{ "St",       NULL,       NULL,       	    0,            0,           0,         1,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
 	{ TERMCLASS,      "bg",        NULL,       	    1 << 7,       0,           1,         0,        -1 },
