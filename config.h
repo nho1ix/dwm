8fade07311ce7a62ffe13d03b30e748c9f1b1f37
Victor Vo <fromvictorvo@gmail.com>
2024-04-09 15:08:34 -0700

    Refactoring / refining config file more.

--- config.h
+++ config.h
@@ -9,19 +9,19 @@ static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 6;       /* snap pixel */
 static const unsigned int systraypinning   = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft    = 0;  	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing   = 0;   /* systray spacing */
-static const int systraypinningfailfirst   = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const unsigned int systrayspacing   = 5;   /* systray spacing */
 static const unsigned int systrayiconsize  = 25;  /* systray icon size in px */
-static const int showsystray  = 1;       /* 0 means no systray */
+static const int systraypinningfailfirst   = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const unsigned int showsystray  = 1;       /* 0 means no systray */
 static unsigned int gappih    = 0;       /* horiz inner gap between windows. default=15 */
 static unsigned int gappiv    = 0;       /* vert inner gap between windows. default=10 */
 static unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge. default=20 */
 static unsigned int gappov    = 0;       /* vert outer gap between windows and screen edge. default=20 */
-static int swallowfloating    = 0;       /* 1 means swallow floating windows by default */
-static int smartgaps          = 0;       /* 1 means no outer gap when there is only one window */
-static int showbar            = 1;       /* 0 means no bar */
-static int topbar             = 1;       /* 0 means bottom bar */
-static int mon_refresh 				= 165;     /* monitor refresh rate */
+static unsigned int swallowfloating    = 0;       /* 1 means swallow floating windows by default */
+static unsigned int smartgaps          = 0;       /* 1 means no outer gap when there is only one window */
+static unsigned int showbar            = 1;       /* 0 means no bar */
+static unsigned int topbar             = 1;       /* 0 means bottom bar */
+static const unsigned int mon_refresh  = 165;     /* monitor refresh rate */
 static const int horizpadbar        = 0;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 15;        /* vertical padding for statusbar */
 static const int vertpad = 0;     /* vertical padding of bar */
@@ -39,13 +39,13 @@ static char selbgcolor[]            = "#212126";  // selected bg tag color
 static char textbgcolor[]           = "#b3b3b3";  // unselected monitor text
 static char *colors[][3] = {
        /*               fg           bg           border   */
-       [SchemeNorm] 		= { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeNorm] 		= { normfgcolor, normbgcolor,  normbordercolor },
        [SchemeSel]  		= { selfgcolor,  normfgcolor,  selbordercolor  },
-       [SchemeStatus] 	= { selfgcolor, normbgcolor, selbordercolor }, // Statusbar right
-       [SchemeTagsSel] 	= { selfgcolor, selbgcolor, selbordercolor }, // Tagbar left selected
-       [SchemeTagsNorm] = { selfgcolor, normbgcolor, selbordercolor }, // Tagbar left unselected
-       [SchemeInfoSel] 	= { selfgcolor, normbgcolor, selbordercolor }, // infobar middle selected
-       [SchemeInfoNorm] = { textbgcolor, normbgcolor, selbordercolor }, // infobar middle unselected
+       [SchemeStatus] 	= { selfgcolor,  normbgcolor,  selbordercolor }, // Statusbar right
+       [SchemeTagsSel] 	= { selfgcolor,  selbgcolor,   selbordercolor }, // Tagbar left selected
+       [SchemeTagsNorm] = { selfgcolor,  normbgcolor,  selbordercolor }, // Tagbar left unselected
+       [SchemeInfoSel] 	= { selfgcolor,  normbgcolor,  selbordercolor }, // infobar middle selected
+       [SchemeInfoNorm] = { textbgcolor, normbgcolor,  selbordercolor }, // infobar middle unselected
 };
 
 typedef struct {
@@ -59,8 +59,29 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "tv", "yt", "www", "chat", "irc", "dev", "music", "sys", "rec" };
-static const char *alttags[] = { "[tv]", "[yt]", "[www]", "[chat]", "[irc]", "[dev]", "[music]", "[sys]", "[rec]" };
+static const char *tags[] = { 
+	"一", // 1st
+	"二", // 2nd
+	"三", // 3rd
+	"四", // 4th
+	"五", // 5th
+	"六", // 6th
+	"七", // 7th
+	"八", // 8th
+	"九"  // 9th
+};
+
+static const char *alttags[] = { 
+	"[一]", // 1st
+	"[二]", // 2nd 
+	"[三]", // 3rd 
+	"[四]", // 4th 
+	"[五]", // 5th 
+	"[六]", // 6th 
+	"[七]", // 7th 
+	"[八]", // 8th 
+	"[九]"  // 9th
+};
 
 static char *tagsel[][2] = {
 	{ "#ca9ee6", selbgcolor },
@@ -86,7 +107,6 @@ static const Rule rules[] = {
 	{ "Spotify",  NULL,       NULL,             1 << 6,       0,           0,         1,         0,        1 },
 	{ "discord",  NULL,       NULL,             1 << 3,       0,           0,         0,         0,        1 },
 	{ "obs",      NULL,       NULL,       	    1 << 8,       0,           0,         1,         0,        1 },
-	{ "obsidian",      NULL,       NULL,   	    1 << 4,       0,           0,         1,         0,        -1 },
 	{ "St",       NULL,       "weechat",       	    1 << 4,            0,           0,         1,         0,        1 },
 	{ "St",       NULL,       NULL,       	    0,            0,           0,         1,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
@@ -98,7 +118,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static float mfact     = 0.5; /* factor of master area size [0.05..0.95] */
 static int nmaster     = 1;    /* number of clients in master area */
-static int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
+static int resizehints = 0;    /* 1 means respect program default minimum window size. Default = 0 */
 
 static int lpm[] = {
         /* Index of preferred layout], if LENGTH(lpm)<#monitors -> default layout */
@@ -137,7 +157,6 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-#define SHCMDno(cmd) { .v = (const char*[]){ cmd, NULL } }
 
 /* commands */
 static const char *termcmd[]  = { TERMINAL, NULL };
@@ -203,11 +222,11 @@ static const Key keys[] = {
 	/* DWM Layouts */
 	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} }, /* tile */
 	{ MODKEY|ShiftMask,		XK_t,		setlayout,	{.v = &layouts[1]} }, /* bstack */
-	{ MODKEY,			XK_u,		setlayout,	{.v = &layouts[4]} }, /* deck */
-	{ MODKEY|ShiftMask,		XK_u,		setlayout,	{.v = &layouts[5]} }, /* monocle */
-	{ MODKEY,			XK_i,		setlayout,	{.v = &layouts[6]} }, /* centeredmaster */
-	{ MODKEY|ShiftMask,		XK_i,		setlayout,	{.v = &layouts[7]} }, /* centeredfloatingmaster */
-	{ MODKEY|ShiftMask,		XK_f,		setlayout,	{.v = &layouts[8]} }, /* fullscreen */
+	{ MODKEY,			XK_u,		setlayout,	{.v = &layouts[2]} }, /* deck */
+	{ MODKEY|ShiftMask,		XK_u,		setlayout,	{.v = &layouts[3]} }, /* monocle */
+	{ MODKEY,			XK_i,		setlayout,	{.v = &layouts[4]} }, /* centeredmaster */
+	{ MODKEY|ShiftMask,		XK_i,		setlayout,	{.v = &layouts[5]} }, /* centeredfloatingmaster */
+	{ MODKEY|ShiftMask,		XK_f,		setlayout,	{.v = &layouts[6]} }, /* fullscreen */
 
 	/* Console / Terminal Programs */
 	{ MODKEY,			XK_Return,	spawn,		{.v = termcmd } },
